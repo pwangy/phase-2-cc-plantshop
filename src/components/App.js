207daab010cf66b0222eee84d32ff1d6e3d6375b
pwangy <blipsandclicks@gmail.com>
2024-02-28 23:27:12 -0800

    clean code

--- src/components/App.js
+++ src/components/App.js
@@ -9,22 +9,4 @@ const App = () => {
 		</div>
 )}
 
-//! Advanced Deliverables
-// update the price of a plant and still see the updated price after refreshing the page.
-// PATCH /plants/:id
-// Required Headers:
-// {
-//   "Content-Type": "application/json"
-// }
-// Request Object:
-// {
-//   "price": number
-// }
-// Example Response:
-// {
-//   "id": 1,
-//   "name": "Aloe",
-//   "image": "./images/aloe.jpg",
-//   "price": 16.99
-// }
 export default App
